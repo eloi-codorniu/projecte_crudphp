0f553a07e2f7c6a6997577341a3e4437722b50f9
eloi-codorniu <ecodorniu@ies-eugeni.cat>
2024-02-16 17:46:51 +0100

    productes no categoria

--- productes.php
+++ productes.php
@@ -1,8 +1,4 @@
-<!-- ======= Our Portfolio Section ======= -->
-
-
 <?php include('connexio_woo.php'); ?>
-
 
 <section id="portfolio" class="portfolio section-bg">
   <div class="container" data-aos="fade-up" data-aos-delay="100">
@@ -23,143 +19,25 @@
       </div>
     </div>
 
-
-    <?php
-    include('connexio_woo.php');
-
-    $productes = $woocommerce->get('products');
-
-    foreach ($productes as $producte) {
-      echo '<div class="col-lg-4 col-md-6 portfolio-item filter-app">';
-      echo '<div class="portfolio-wrap">';
-      echo '<img src="' . $producte->images[0]->src . '" class="img-fluid" alt="' . $producte->name . '">';
-      echo '<div class="portfolio-info">';
-      echo '<h4>' . $producte->name . '</h4>';
-
-      echo '<p>' . $producte->categories[0]->name . '</p>';
-      echo '<div class="portfolio-links">';
-      echo '<a href="' . $producte->images[0]->src . '" data-gallery="portfolioGallery" class="portfolio-lightbox" title="' . $producte->name . '"><i class="bi bi-plus"></i></a>';
-
-      echo '</div>';
-      echo '</div>';
-      echo '</div>';
-      echo '</div>';
-    }
-    ?>
-
-    <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bi bi-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>App 2</h4>
-                <p>App</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bi bi-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Card 2</h4>
-                <p>Card</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bi bi-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Web 2</h4>
-                <p>Web</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bi bi-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>App 3</h4>
-                <p>App</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 3"><i class="bi bi-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Card 1</h4>
-                <p>Card</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 1"><i class="bi bi-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Card 3</h4>
-                <p>Card</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 3"><i class="bi bi-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bi bi-plus"></i></a>
-                  <a href="portfolio-details.html" title="More Details"><i class="bi bi-link"></i></a>
-                </div>
-              </div>
-            </div>
-          </div> -->
-
-  </div>
-
+    <div class="row">
+      <?php
+      include('connexio_woo.php');
+      $productes = $woocommerce->get('products');
+      foreach ($productes as $producte) {
+        echo '<div class="col-lg-3 col-md-4 col-sm-6 portfolio-item filter-app">';
+        echo '<div class="portfolio-wrap">';
+        echo '<img src="' . $producte->images[0]->src . '" class="img-fluid" alt="' . $producte->name . '">';
+        echo '<div class="portfolio-info">';
+        echo '<h4>' . $producte->name . '</h4>';
+        echo '<p>' . $producte->categories[0]->name . '</p>';
+        echo '<div class="portfolio-links">';
+        echo '<a href="' . $producte->images[0]->src . '" data-gallery="portfolioGallery" class="portfolio-lightbox" title="' . $producte->name . '"><i class="bi bi-plus"></i></a>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+      }
+      ?>
+    </div>
   </div>
 </section><!-- End Our Portfolio Section -->
