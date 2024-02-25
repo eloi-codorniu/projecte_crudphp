8b94fa11ba5a1b67e9cbf22d2913837a7d2ccb9b
eloicodorniu <eloicodorniu@gmail.com>
2024-02-25 12:18:12 +0100

    SERVEI ACTUALITZAR OK

--- servei_actualitzar_id.php
+++ servei_actualitzar_id.php
@@ -1,17 +1,17 @@
+<?php 
+include('connexio_woo.php');
 
-<?php include('connexio_woo.php');
+$id_Producte = $_POST['id'];
+$nom_producte = $_POST['name'];
+$descripcio = $_POST['short_description'];
 
-      $id_Producte = $_POST['id'];
-     $nom_producte = $_POST['name'];
-    $descripcio = $_POST['short_description'];
+$data = [
+    'name' => $nom_producte,
+    'short_description' => $descripcio,
+];
 
+$woocommerce->put('products/' . $id_Producte, $data);
 
-     $data = [ 'name' => $nom_producte,
-                        'short_description' => $descripcio, ];
-
-                    $woocommerce->put('products/' . $id_Producte, $data);
-
-                   header("refresh:2; url=index.php#portfolio");
-                    exit();
-               
-                ?>
+header("refresh:0; url=index.php");
+exit();
+?>
