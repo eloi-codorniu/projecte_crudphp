a25ad40a00a92732306af9431fc25b3dc92def19
eloi-codorniu <ecodorniu@ies-eugeni.cat>
2024-02-26 17:42:01 +0100

    SERVEI BORRAR OK

--- servei_esborrar_id.php
+++ servei_esborrar_id.php
@@ -1,17 +1,13 @@
-<?php 
+<?php
 include('connexio_woo.php');
 
 $id_Producte = $_POST['id'];
-$nom_producte = $_POST['name'];
-$descripcio = $_POST['short_description'];
 
-$data = [
-    'name' => $nom_producte,
-    'short_description' => $descripcio,
-];
+$woocommerce->delete('products/' . $id_Producte);
 
- $woocommerce->delete('products/794', ['force' => true]);
 
-header("refresh:0; url=index.php");
+
+header("refresh:2; url=index.php");
 exit();
+
 ?>
